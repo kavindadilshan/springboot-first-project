58a8b50b18948d8d271e215084a615ad19b0fd1e
kavindadilshan <kavindadilshan97@gmail.com>
2021-05-10 13:29:20 +0530

    created new resources using post

--- src/main/java/com/example/demo/topic/TopicController.java
+++ src/main/java/com/example/demo/topic/TopicController.java
@@ -1,9 +1,7 @@
 package com.example.demo.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,5 +20,10 @@ public class TopicController {
     @RequestMapping("/topic/{id}")
     public Topic getTopic(@PathVariable String id){
         return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value="/topic")
+    public void addTopic (@RequestBody Topic topic){
+        topicService.addTopic(topic);
     }
 }
